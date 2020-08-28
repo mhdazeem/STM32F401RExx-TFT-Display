c31b1875f98e70ea916f54bc49d06e310f70a0ea
mhdazeem <mhd.azeem@hotmail.com>
2020-08-28 10:23:37 +0100

    Initial Commit
    Initial Commit @ Muhammad Azeem

---
+++ Core/Inc/mxconstants.h
@@ -0,0 +1,62 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MXCONSTANT_H
+#define __MXCONSTANT_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+#define T_IRQ_Pin GPIO_PIN_2
+#define T_IRQ_GPIO_Port GPIOA
+
+#define T_CLK_Pin GPIO_PIN_5		// SCK	PA_5(D13)
+#define T_CLK_GPIO_Port GPIOA
+
+#define T_MISO_Pin GPIO_PIN_6		// MISO	PA_6(D12)
+#define T_MISO_GPIO_Port GPIOA
+
+#define T_MOSI_Pin GPIO_PIN_7		// MOSI	PA_7(D11)
+#define T_MOSI_GPIO_Port GPIOA
+
+#define T_CS_Pin GPIO_PIN_5			// CS	PB_5(D4)
+#define T_CS_GPIO_Port GPIOB
+
+#define DC_Pin GPIO_PIN_8			// D/C	PA_8(D7)
+#define DC_GPIO_Port GPIOA
+
+#define RST_Pin GPIO_PIN_10			// Reset	PA_10(D2) Pull Up(100k)
+#define RST_GPIO_Port GPIOA
+
+#endif /* __MXCONSTANT_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
